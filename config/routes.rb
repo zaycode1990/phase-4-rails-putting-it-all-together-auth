08ad2ce182385ad5b3ce4678c5191f3120b6f525
Isaiah <ijandicos90@gmail.com>
2022-09-03 21:16:25 -0400

    commit tree 2664a505b084dd42aa3cc96d80b45ce03b6f3097

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+resources :recipes, only: [:index, :create]
+  get "/me", to: "users#show"
+  get "/recipes", to: "recipes#index"
+  post "/signup", to: "users#create"
+  post "/login", to: "sessions#create"
+  post "recipes", to: "recipes#create"
+  delete "/logout", to: "sessions#destroy"
+
+
+   
+
 end
